055e5521a060de3d46e7ac48363d8ae74bcd231d
marcosduarte356 <marcosduarte356@gmail.com>
2026-06-10 16:25:15 -0300

    v6.2: 3 zonas horizontais (Azul 1x-2x, Roxa 2x-10x, Rosa 10x+) + remocao de indicadores (bandeira, fundo, resistencia, sinal, cruzamento MM)

---
+++ extensao/background.js
@@ -0,0 +1,14 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.alarms.create("keepAlive", { periodInMinutes: 1 });
+  console.log("[BG] Extensao Sortenabet iniciada");
+});
+
+chrome.alarms.onAlarm.addListener((alarm) => {
+  if (alarm.name === "keepAlive") {
+    console.log("[BG] Alive");
+  }
+});
+
+setInterval(() => {
+  fetch("https://aviator-trader.onrender.com/api/status").catch(() => {});
+}, 4 * 60 * 1000);
